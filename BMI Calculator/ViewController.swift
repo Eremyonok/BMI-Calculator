145bc49e92bd4a69645612dc8c2e72e9e9fbc364
Elizaveta <eremeonock.el@gmail.com>
2023-09-26 17:10:35 +0300

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Elizaveta Eremyonok on 26.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
