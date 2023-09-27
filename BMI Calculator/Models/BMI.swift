9d2c3c716844496ac3348bc4d57ed3c080790510
Elizaveta <eremeonock.el@gmail.com>
2023-09-27 11:00:57 +0300

    Add new functionality

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Elizaveta Eremyonok on 26.09.2023.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
